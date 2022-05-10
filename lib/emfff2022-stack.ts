e36e295bb841b223afe94c4d10cd694bd11f9d57
PJ Evans <pj@mrpjevans.com>
2022-05-10 21:55:29 +0100

    Initial commit

--- lib/emfff2022-stack.ts
+++ lib/emfff2022-stack.ts
@@ -1,16 +1,22 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class Emfff2022Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // defines an AWS Lambda resource
+    const programme = new lambda.Function(this, "programme", {
+      runtime: lambda.Runtime.NODEJS_14_X,    // execution environment
+      code: lambda.Code.fromAsset('lambdas/programme'),
+      handler: 'programme.handler'                // file is "hello", function is "handler"
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Emfff2022Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, "programme-endpoint", {
+      handler: programme
+    });  
   }
 }
