3023dd03e21dd3291f266b958cb4d66532b02c91
PJ Evans <pj@mrpjevans.com>
2022-05-16 21:30:51 +0100

    Initial commit

---
+++ lambdas/feed/feed.ts
@@ -0,0 +1,32 @@
+import { promises as fs } from "fs"
+
+exports.handler = async function(event) {
+    const stage = event.requestContext.stage;
+    const now = new Date();
+    const day = event.queryStringParameters?.day ?? now.getDay();
+    const dayMap = [
+      "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    ];
+    const today = dayMap[day];
+    const filter = event.queryStringParameters?.filter ?? "all";
+    const ts = event.queryStringParameters?.time ?? now.getTime();
+
+    const raw = await (await fs.readFile(`./feed_${stage}.json`, "utf-8")).toString();
+    const json = JSON.parse(raw);
+    let filteredJson = json;
+
+    switch(filter) {
+      case "today":
+        filteredJson.films = json.films.filter(film => film.showing.day == today);
+        break;
+      case "future":
+        filteredJson.films = json.films.filter(film => film.showing.timestamp >= ts);
+        break;
+    };
+
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(filteredJson)
+    };
+}
--- lib/emfff2022-stack.ts
+++ lib/emfff2022-stack.ts
@@ -2,21 +2,42 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 
 export class Emfff2022Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const STAGE = process.env.STAGE || 'dev'
+
     // defines an AWS Lambda resource
-    const programme = new lambda.Function(this, "programme", {
+    const feed = new lambda.Function(this, "feed", {
       runtime: lambda.Runtime.NODEJS_14_X,    // execution environment
-      code: lambda.Code.fromAsset('lambdas/programme'),
-      handler: 'programme.handler'                // file is "hello", function is "handler"
+      code: lambda.Code.fromAsset('lambdas/feed'),
+      handler: 'feed.handler'                // file is "hello", function is "handler"
     });
 
     // defines an API Gateway REST API resource backed by our "hello" function.
-    new apigw.LambdaRestApi(this, "programme-endpoint", {
-      handler: programme
-    });  
+    //const apigwDeployment = new apigw.LambdaRestApi(this, "feed-endpoint", {
+    //  handler: programme
+    //});
+
+    // IMPORTANT: Lambda grant invoke to APIGateway
+    feed.grantInvoke(new ServicePrincipal('apigateway.amazonaws.com'));
+
+    // Then, create the API construct, integrate with lambda
+    const api = new apigw.RestApi(this, 'feed_api', { deploy: false });
+    const integration = new apigw.LambdaIntegration(feed);
+    api.root.addMethod('GET', integration)
+
+    // Then create an explicit Deployment construct
+    const deployment  = new apigw.Deployment(this, 'feed_deployment', { api });
+
+    // And different stages
+    const [devStage, prodStage] = ['dev', 'v1'].map(item => 
+      new apigw.Stage(this, `${item}_stage`, { deployment, stageName: item }));
+
+    api.deploymentStage = prodStage
+
   }
 }
